343e2f2f33828757bca92eedcf9e95e8cd0ac574
Júlio Barbosa <julio.barbosa@evoluitecnologia.com.br>
2022-05-11 00:12:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const PORT = 8181;
+
+app.listen(
+    PORT,
+    () => console.log(`it is alive on http://localshot:${PORT}`)
+);
+
+app.get('/', (request, response) => {
+    response.status(200).send({
+        "nome": "julio",
+        "idade": 36
+    });
+});
